95063cc75f2f603c05a30d114397eaafa494b7ca
python-clojure <megha__akash@outlook.com>
2021-04-16 22:08:29 +0530

    Changed route from /users/sign_in to /login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-mount Bs5::Engine => '/bs5'
+  devise_scope :user do
+    get 'login' to: 'devise/sessions#new'
+  end
   root to: 'pages#index'
 end
